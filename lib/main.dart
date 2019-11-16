b428989ed5452b99d29b587351b801bb6e883ce3
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-11-16 14:39:30 +0530

    :art: Appbar with leading icons and actions

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: FlutterLogo(
+          colors: Colors.green,
+          size: 30.0,
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
+          color: Colors.grey
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+            color: Colors.grey
+          )
+        ],
+      ),
     );
   }
 }
